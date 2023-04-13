d1eac295ea01bb53bc94fdcfff212cda335a53c1
“xiaou” <15901797213@163.com>
2023-04-13 17:54:46 +0800

    逆向工程生成代码

---
+++ service/service_edu/src/main/java/com/xiaou/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.xiaou.eduservice.service;
+
+import com.xiaou.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2023-04-13
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/xiaou/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.xiaou.eduservice.service.impl;
+
+import com.xiaou.eduservice.entity.EduTeacher;
+import com.xiaou.eduservice.mapper.EduTeacherMapper;
+import com.xiaou.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2023-04-13
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
